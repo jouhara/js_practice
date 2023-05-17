2f76698e49a38b780d33fccdf4cea83014b994b5
Jouhara.NK <nkjouhara@gmail.com>
2023-05-17 19:23:43 +0530

    day1

---
+++ interaction.js
@@ -0,0 +1,5 @@
+alert("hello")// alert is synchronous, bcs after cliking ok only remining code wrks
+let res=prompt("enter yur nme");
+console.log(res)
+let isDelete=confirm('ae yu syre?');
+console.log(isDelete)
---
+++ var.js
@@ -0,0 +1,6 @@
+let result=10;//js can take any typ of val
+console.log(result);
+result="hello";
+console.log(result);
+result=true;
+console.log(result)
